46562774694139a95d61a826df3c162ff87a7436
Greg Barendt <gregory.barendt@icloud.com>
2014-03-30 16:30:16 -0400

    Add a script to help with GO term selection

---
+++ go_term_selection.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# This script searches a Gene Ontology OBO file and returns a
+# CSV-formatted list of ID and names for terms that include
+# one of the defined KEYWORDS
+
+require 'gene_ontology'
+
+KEYWORDS = %w{immun vir antibacteria defens}
+
+go = GeneOntology.new.from_file('data/gene_ontology_ext.obo')
+go.id_to_term.each do |term|
+  KEYWORDS.each do |keyword|
+    if term[1].name.include?(keyword)
+      puts "#{term[1].id},#{term[1].name}"
+      break
+    end
+  end
+end
