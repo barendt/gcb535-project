a999ef4293ee109921d109f16d0c2c66b9927e24
Greg Barendt <gregory.barendt@icloud.com>
2014-04-13 23:46:23 -0400

    Add specs for SpiderTranscript

--- spec/models/spider_transcript_spec.rb
+++ spec/models/spider_transcript_spec.rb
@@ -9,5 +9,34 @@ describe SpiderTranscript do
     SpiderTranscript.new
   end
 
+  context 'Finding a CDS' do
+
+    it 'should replace T with U' do
+      subject.transcript = 'ATG'
+      subject.transcript.should == 'AUG'
+    end
+
+    it 'should strip any newline characters in the transcript' do
+      subject.transcript = "ATG\nGGA"
+      subject.transcript.should == 'AUGGGA'
+    end
+
+    it 'should raise an exception if no start codon is found' do
+      lambda {
+        subject.transcript = 'UGAAAGU' 
+      }.should raise_exception(StartCodonNotFoundError)
+    end
+
+    it 'should have the translated CDS' do
+      subject.transcript = 'ATGGGATCATGA'
+      subject.translated_sequence.should == 'MGS*'
+    end
+
+    it 'should translate only to the first stop codon' do
+      subject.transcript = 'ATGGGATCATGA'*2
+      subject.translated_sequence.should == 'MGS*'
+    end
+
+  end
 
 end
