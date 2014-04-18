dbdf93f267782176eed4397a2a20f857e0163cfa
Greg Barendt <gregory.barendt@icloud.com>
2014-04-17 22:49:01 -0400

    Add BlastHit#to_csv instance method

--- lib/models/blast_hit.rb
+++ lib/models/blast_hit.rb
@@ -1,5 +1,9 @@
 class BlastHit < ActiveRecord::Base
 
+  def to_csv
+    "#{target_species},#{target_id},#{query_species},#{query_id}"
+  end
+
   def uniprot_id
     query_id.split('|')[1]
   end
--- spec/models/blast_hit_spec.rb
+++ spec/models/blast_hit_spec.rb
@@ -11,4 +11,14 @@ describe BlastHit do
     subject.uniprot_id.should == 'Q9Y572'
   end
 
+  it 'can return itself as a CSV-formatted string' do
+    subject.stub(:query_species).and_return('Homo sapiens')
+    subject.stub(:query_id).and_return('sp|Q9Y572|RIPK3_HUMAN')
+    subject.stub(:target_species).and_return('Nep')
+    subject.stub(:target_id).and_return('comp1733840_c1_seq7')
+
+    expected_csv = 'Nep,comp1733840_c1_seq7,Homo sapiens,sp|Q9Y572|RIPK3_HUMAN'
+    subject.to_csv.should == expected_csv
+  end
+
 end
