fbf0ccf4b8eec5d8a39a1a9b558090754753ce47
Greg Barendt <gregory.barendt@icloud.com>
2014-04-26 16:17:53 -0400

    Also generate CSV files containing individual hits for each GO term

--- visualization/generate_go_chart_data.rb
+++ visualization/generate_go_chart_data.rb
@@ -2,6 +2,7 @@
 
 require_relative '../environment'
 
+require 'fileutils'
 require 'gchart'
 require 'trollop'
 
@@ -23,10 +24,17 @@ end
 end
 
 go_term_counts = Hash.new(0)
+individual_hits = Hash.new
 
 BlastHit.where(query_taxon: opts[:querytaxon], blastdb: opts[:blastdb]).each do |hit|
   GoaLine.where(taxon: opts[:querytaxon], db_object_id: hit.product_id).each do |gl|
     go_term_counts[gl.go_id] += 1
+    term = GoTerm.where(go_id: gl.go_id).first
+    if individual_hits.has_key?(gl.go_id)
+      individual_hits[gl.go_id] << gl.db_object_name
+    else
+      individual_hits[gl.go_id] = [gl.db_object_name]
+    end
   end
 end
 
@@ -35,6 +43,23 @@ csv_file = File.join($app_root, 'results', "gochartdata-#{opts[:querytaxon]}_#{t
 File.open(csv_file, 'w') do |f|
   go_term_counts.each do |k, v|
     gt = GoTerm.where(go_id: k).first
-    f.write "\"#{gt.go_term}\",\"#{v}\"\n"
+    f.write "\"#{k}\",\"#{gt.go_term}\",\"#{v}\"\n"
   end
 end
+
+term_results_path = File.join($app_root, 'results', 'go_terms', "#{opts[:querytaxon]}_#{target_taxon}")
+unless File.exists? term_results_path
+  FileUtils::mkdir_p term_results_path
+  individual_hits.each do |go_id, objects|
+    term_file = File.join(term_results_path, "#{go_id.split(':').last}.csv")
+    term = GoTerm.where(go_id: go_id).first.go_term
+    File.open(term_file, 'w') do |f|
+      f.write("\"#{go_id}\",\"term\"\n")
+      objects.each do |object|
+        f.write("\"#{object}\"\n")
+      end
+    end
+  end
+end
+
+
